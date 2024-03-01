edae3c0004e19dcba999e9499998e92e0bf84e8b
Gaurav Gogoi <gauravgogoigp@gmail.com>
2024-03-01 21:30:29 +0530

    version 2

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
-const express = require("express")
+import express from "express"
+import bodyParser from "body-parser"
 
 const app = express()
 
 const port = 8080
 
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.get("/home/:item" , (req, resp) => {
+    
+    const item = req.params.item
+    console.log(item)
+    if(item == 1){
+        resp.send("hi")
+    }
+    else{
+        resp.send("wrong item")
+    }
+    
+})
+
+app.get("/home", (req, resp)=>{
+
+
+    const item = req.query.username
+    if(item){
+        resp.send(`Hi ${item}`)
+    }
+    else{
+        resp.send('send a name as username')
+    }
+}
+
+    
+
+)
+
+
 app.listen(port, () => {
     console.log("App listens on ", port)
-})
-
-app.get("/home" , (req, resp) => {
-    console.log("request sent to browser")
-    resp.send("Hello from server")
-})
-
-app.post("/home", (req,resp) => {
-    console.log("browser requested for post api")
-    resp.send("Hello request")
 })
